b28d4243f4e2fef4fefd6e968c105156c7ae7ed6
Jarred Capellman <jcapellman@hotmail.com>
2019-01-06 00:26:15 -0600

    Added: GetLabelAttributes to the ExtensionMethods class

--- mldeepdivelib/Common/ExtensionMethods.cs
+++ mldeepdivelib/Common/ExtensionMethods.cs
@@ -13,5 +13,30 @@ namespace mldeepdivelib.Common
 
             return fields.Select((t, x) => new TextLoader.Column(t.Name, t.PropertyType == typeof(string) ? DataKind.Text : DataKind.R4, x)).ToArray();
         }
+
+        public static (string Name, int Min, int Max) GetLabelAttributes<T>(this T model)
+        {
+            var fields = model.GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var field in fields)
+            {
+                if (field.CustomAttributes.All(a => a.AttributeType != typeof(LabelAttribute)))
+                {
+                    continue;
+                }
+
+                var attribute =
+                    (LabelAttribute) field.GetCustomAttributes().FirstOrDefault(a => a.GetType() == typeof(LabelAttribute));
+
+                if (attribute == null)
+                {
+                    continue;
+                }
+
+                return (field.Name, attribute.Min, attribute.Max);
+            }
+
+            return (null, -1, -1);
+        }
     }
 }
