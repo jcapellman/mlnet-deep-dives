117d0db3332249ca7c46eee8ed648937547f9efa
Jarred Capellman <jcapellman@hotmail.com>
2019-01-06 00:29:00 -0600

    Added: LabelAttribute class

---
+++ mldeepdivelib/Common/LabelAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace mldeepdivelib.Common
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class LabelAttribute : Attribute
+    {
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+        public LabelAttribute(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
