84fb1a69dd769e5d1c2a434132ee6bd0aeb09519
Jarred Capellman <jcapellman@hotmail.com>
2019-01-19 11:47:36 -0600

    Added: Initial checkin of the ThreatPredictor class

---
+++ ThreatClassifier/Structures/ThreatPredictor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+using mldeepdivelib.Common;
+
+namespace ThreatClassifier.Structures
+{
+    public class ThreatPredictor
+    {
+        [Label(0, 150)]
+        public uint ThreatClusterId { get; set; }
+
+        [ColumnName("Score")]
+        public float[] Distances { get; set; }
+    }
+}
