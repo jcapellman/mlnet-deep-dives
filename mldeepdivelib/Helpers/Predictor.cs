be06a848452ecbe107a289d9d79c07a413e1a0b7
Jarred Capellman <jcapellman@hotmail.com>
2019-01-20 11:29:39 -0600

    Added: Ability to classify just passing in the filepath of the PE32 file

--- mldeepdivelib/Helpers/Predictor.cs
+++ mldeepdivelib/Helpers/Predictor.cs
@@ -8,14 +8,8 @@ namespace mldeepdivelib.Helpers
 {
     public static class Predictor
     {
-        public static TK Predict<T, TK>(MLContext mlContext, string modelPath, string predictionFilePath) where T : class where TK : class, new()
+        public static TK Predict<T, TK>(MLContext mlContext, string modelPath, T predictionData) where T : class where TK : class, new()
         {
-            var data = File.ReadAllText(predictionFilePath);
-
-            var predictionData = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(data);
-
-            Console.WriteLine($"Data:{Environment.NewLine}{data}{Environment.NewLine}");
-
             ITransformer trainedModel;
 
             using (var stream = new FileStream(modelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
@@ -26,6 +20,15 @@ namespace mldeepdivelib.Helpers
             var predFunction = trainedModel.CreatePredictionEngine<T, TK>(mlContext);
 
             return predFunction.Predict(predictionData);
+        }
+
+        public static TK Predict<T, TK>(MLContext mlContext, string modelPath, string predictionFilePath) where T : class where TK : class, new()
+        {
+            var data = File.ReadAllText(predictionFilePath);
+
+            var predictionData = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(data);
+
+            return Predict<T, TK>(mlContext, modelPath, predictionData);
         }
     }
 }
