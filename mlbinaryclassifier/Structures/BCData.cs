d22a8df2ffbb295fdf2f377da401d4fa25d4f03e
Jarred Capellman <jcapellman@hotmail.com>
2019-01-21 20:30:15 -0600

    Added: Initial commit of the BCData class

---
+++ mlbinaryclassifier/Structures/BCData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+
+namespace mlbinaryclassifier.Structures
+{
+    public class BCData
+    {
+        [Column(ordinal: "0", name: "Label")]
+        public float StillThere { get; set; }
+
+        [Column(ordinal: "1")]
+        public float LongCommute { get; set; }
+        
+        [Column(ordinal: "2")]
+        public float PromotionLimited { get; set; }
+        
+        [Column(ordinal: "3")]
+        public float Overwhelmed { get; set; }
+    }
+}
