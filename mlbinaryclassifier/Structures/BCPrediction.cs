ee39e4634058610b23def851a786e4c1aa3b4145
Jarred Capellman <jcapellman@hotmail.com>
2019-01-21 20:30:41 -0600

    Added: Initial commit of the BCPrediction class

---
+++ mlbinaryclassifier/Structures/BCPrediction.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+
+namespace mlbinaryclassifier.Structures
+{
+    public class BCPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        [ColumnName("Probability")]
+        public float Probability { get; set; }
+
+        [ColumnName("Score")]
+        public float Score { get; set; }
+    }
+}
