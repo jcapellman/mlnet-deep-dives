c0430cc2ad635624c73aa4ce4d24e5d8d5933203
Jarred Capellman <jcapellman@hotmail.com>
2019-02-06 21:40:19 -0600

    Added: Initial commit of the JpegArtifactorDetectorData

---
+++ mljpegartifactdetector/Structures/JpegArtifactorDetectorData.cs
@@ -0,0 +1,14 @@
+﻿using mldeepdivelib.Common;
+
+namespace mljpegartifactdetector.Structures
+{
+    public class JpegArtifactorDetectorData
+    {
+        [Label(int.MinValue, int.MaxValue)]
+        public float ContainsJpegArtifacts { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public override string ToString() => $"0.0f, {System.Text.Encoding.UTF8.GetString(Data)}";
+    }
+}
