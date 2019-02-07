b2dd6262d84dfa7e547eaac67d759910c2e10b4d
Jarred Capellman <jcapellman@hotmail.com>
2019-02-06 20:42:27 -0600

    Added: Initial commit of the jpegdetector class

---
+++ mljpegartifactdetector/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace mljpegartifactdetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new jpegdetector().Run(args);
+        }
+    }
+}
---
+++ mljpegartifactdetector/jpegdetector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using mldeepdivelib.Abstractions;
+
+namespace mljpegartifactdetector
+{
+    public class jpegdetector : BaseMLPrediction
+    {
+        protected override void Train(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Predict(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void FeatureExtraction(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
