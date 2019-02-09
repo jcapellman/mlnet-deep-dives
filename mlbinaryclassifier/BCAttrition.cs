56514efea7a4457c3966afb515b09ddf41524b69
Jarred Capellman <jcapellman@hotmail.com>
2019-02-09 08:43:09 -0600

    Change: BCAttrition class refactoring to utilize the CommandLineArguments Enum

--- mlbinaryclassifier/BCAttrition.cs
+++ mlbinaryclassifier/BCAttrition.cs
@@ -4,6 +4,7 @@ using System.IO;
 using mlbinaryclassifier.Structures;
 
 using mldeepdivelib.Abstractions;
+using mldeepdivelib.Enums;
 using mldeepdivelib.Helpers;
 
 using Microsoft.ML;
@@ -25,31 +26,26 @@ namespace mlbinaryclassifier
                 }
             });
 
-            var baseTrainingDataView = textReader.Read(args[1]);
+            var baseTrainingDataView = textReader.Read(args[(int)CommandLineArguments.INPUT_FILE]);
 
             var pipeline = MlContext.Transforms.Text.FeaturizeText("Content", "Features")
                 .Append(MlContext.BinaryClassification.Trainers.FastTree(numLeaves: 2, numTrees: 10, minDatapointsInLeaves: 1));
 
             var trainedModel = pipeline.Fit(baseTrainingDataView);
 
-            using (var fs = File.Create(args[2]))
+            using (var fs = File.Create(args[(int)CommandLineArguments.OUTPUT_FILE]))
             {
                 trainedModel.SaveTo(MlContext, fs);
             }
 
-            Console.WriteLine($"Saved model to {args[2]}");
+            Console.WriteLine($"Saved model to {args[(int)CommandLineArguments.OUTPUT_FILE]}");
         }
 
         protected override void Predict(string[] args)
         {
-            var prediction = Predictor.Predict<BCData, BCPrediction>(MlContext, args[1], args[2]);
+            var prediction = Predictor.Predict<BCData, BCPrediction>(MlContext, args[(int)CommandLineArguments.INPUT_FILE], args[(int)CommandLineArguments.OUTPUT_FILE]);
 
             Console.WriteLine($"Likely not happy: {prediction.Probability * 100:##.#}%");
-        }
-
-        protected override void FeatureExtraction(string[] args)
-        {
-            throw new NotImplementedException();
         }
     }
 }
