d8bcf12cf31fc7794529fdceb9bebe0fccdb55a2
Jarred Capellman <jcapellman@hotmail.com>
2019-02-10 19:04:54 -0600

    Added: Initial commit of the SentimentData class

---
+++ mlgoodreviewsentimentanalysis/Structures/SentimentData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace mlgoodreviewsentimentanalysis.Structures
+{
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public bool Label { get; set; }
+
+        [LoadColumn(1)]
+        public string Text { get; set; }
+    }
+}
