d5ab8672f942f84e173895c48d593f258d59f0a2
Jarred Capellman <jcapellman@hotmail.com>
2019-02-14 20:19:45 -0600

    Added: Initial commit of the random_file_classification class

---
+++ ml_random_file_classification/random_file_classification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using mldeepdivelib.Abstractions;
+
+namespace ml_random_file_classification
+{
+    public class random_file_classification : BaseMLPrediction
+    {
+        protected override void Train(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Predict(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
