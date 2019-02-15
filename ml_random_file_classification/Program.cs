f54f492b14299c4bca957151279a0f5ebba42d02
Jarred Capellman <jcapellman@hotmail.com>
2019-02-14 20:19:15 -0600

    Added: ml_random_file_classification/Program.cs

---
+++ ml_random_file_classification/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ml_random_file_classification
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new random_file_classification().Run(args);
+        }
+    }
+}
