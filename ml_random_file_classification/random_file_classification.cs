2a29b0b67ed44eff14f5782dc324be964c3eb446
Jarred Capellman <jcapellman@hotmail.com>
2019-02-14 21:52:46 -0600

    Added: Train implementation in the random_file_classification class

--- ml_random_file_classification/random_file_classification.cs
+++ ml_random_file_classification/random_file_classification.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.IO;
 
 using mldeepdivelib.Abstractions;
+using mldeepdivelib.Enums;
+
+using ml_random_file_classification.Structures;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
 
 namespace ml_random_file_classification
 {
@@ -8,7 +15,21 @@ namespace ml_random_file_classification
     {
         protected override void Train(string[] args)
         {
-            throw new NotImplementedException();
+            var data = MlContext.Data.ReadFromBinary(path: args[(int)CommandLineArguments.INPUT_FILE]);
+
+            var pipeline = MlContext.Transforms.Text.FeaturizeText(DefaultColumnNames.Features, nameof(FileData.Strings));
+
+            var trainer = MlContext.BinaryClassification.Trainers.FastTree();
+            var trainingPipeline = pipeline.Append(trainer);
+
+            var trainedModel = trainingPipeline.Fit(data);
+
+            using (var fs = File.Create(args[(int)CommandLineArguments.OUTPUT_FILE]))
+            {
+                trainedModel.SaveTo(MlContext, fs);
+            }
+
+            Console.WriteLine($"Saved model to {args[(int)CommandLineArguments.OUTPUT_FILE]}");
         }
 
         protected override void Predict(string[] args)
