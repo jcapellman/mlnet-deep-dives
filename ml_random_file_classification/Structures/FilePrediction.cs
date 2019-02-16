e6d24cc24712eff9d11006e6b6d914ac0021bcb0
Jarred Capellman <jcapellman@hotmail.com>
2019-02-15 20:05:09 -0600

    Added: Initial work on the FilePrediction class

--- ml_random_file_classification/Structures/FilePrediction.cs
+++ ml_random_file_classification/Structures/FilePrediction.cs
@@ -1,6 +1,14 @@
-﻿namespace ml_random_file_classification.Structures
+﻿using Microsoft.ML.Data;
+
+namespace ml_random_file_classification.Structures
 {
     public class FilePrediction
     {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
     }
 }
